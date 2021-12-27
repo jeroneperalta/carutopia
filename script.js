e418ef082cb4b9b885049154016d43930523df2d
Jerone Peralta <jeroneperalta@gmail.com>
2021-12-27 18:13:31 +0800

    Upload Core files

---
+++ script.js
@@ -0,0 +1,59 @@
+function heroSlider() {
+  tns({
+    container: ".hero--slide",
+    autoplay: true,
+    autoplayTimeout: 8000,
+    autoplayButtonOutput: false,
+    mode: "gallery",
+    speed: 3000,
+    controls: false,
+  });
+  const nextSlides = document.querySelectorAll(".hero--slide--item");
+  nextSlides.forEach((element) => element.classList.remove("d-none"));
+}
+function reviewSlider() {
+  tns({
+    container: ".reviews--list",
+    items: 1,
+    navPosition: "bottom",
+    edgePadding: 30,
+    loop: false,
+    rewind: true,
+    speed: 800,
+    controls: false,
+    responsive: {
+      1440: {
+        items: 4,
+        edgePadding: 70,
+      },
+      1024: {
+        items: 3,
+        edgePadding: 60,
+      },
+      768: {
+        items: 2,
+        edgePadding: 50,
+      },
+    },
+  });
+}
+function lightBox() {
+  const lightbox = GLightbox({
+    touchNavigation: true,
+    loop: true,
+    autoplayVideos: true,
+  });
+}
+
+function smoothScroll() {
+  window.addEventListener("load", function () {
+    document
+      .querySelector("#hero--anchor--link")
+      .addEventListener("click", function (e) {
+        e.preventDefault();
+        document.querySelector("#services").scrollIntoView({
+          behavior: "smooth",
+        });
+      });
+  });
+}
